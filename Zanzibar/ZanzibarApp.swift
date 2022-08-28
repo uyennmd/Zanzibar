88f58b2086792fb72141a9b422107041898f505c
Uyen <uyen.nguyenminhduy@gmail.com>
2022-08-28 17:22:11 +0700

    Initial Commit

---
+++ Zanzibar/ZanzibarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZanzibarApp.swift
+//  Zanzibar
+//
+//  Created by Uyen Nguyen Minh Duy on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZanzibarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
