7fadd1737778e2f1dfb3758425712fd7e63649ab
IDS-V-Intern-Uyennmd <67620752+uyennmd@users.noreply.github.com>
2022-08-29 22:21:00 +0700

    Update HowToPlay.swift

--- Zanzibar/Views/HowToPlay.swift
+++ Zanzibar/Views/HowToPlay.swift
@@ -1,10 +1,14 @@
-//
-//  HowToPlay.swift
-//  Zanzibar
-//
-//  Created by Uyen Nguyen Minh Duy on 29/08/2022.
-//
-
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Nguyen Minh Duy Uyen
+  ID: s3819342
+  Created  date: 23/08/2022
+  Last modified: 29/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 import SwiftUI
 
 struct HowToPlay: View {
@@ -13,7 +17,7 @@ struct HowToPlay: View {
             myColor.blue
                 .opacity(0.4)
                 .ignoresSafeArea(.all)
-            ScrollView {
+            ScrollView {//use scroll view for the tutorial
                 VStack{
                     Text("Zanzibar Dice Game Rules")
                         .foregroundColor(.pink)
