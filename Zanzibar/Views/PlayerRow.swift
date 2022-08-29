26d98cd810de1663d85caaa9edd0f8a3eb582b99
IDS-V-Intern-Uyennmd <67620752+uyennmd@users.noreply.github.com>
2022-08-29 22:34:52 +0700

    Update PlayerRow.swift

--- Zanzibar/Views/PlayerRow.swift
+++ Zanzibar/Views/PlayerRow.swift
@@ -1,15 +1,21 @@
-//
-//  PlayerRow.swift
-//  Zanzibar (iOS)
-//
-//  Created by Uyen Nguyen Minh Duy on 28/08/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Nguyen Minh Duy Uyen
+  ID: s3819342
+  Created  date: 23/08/2022
+  Last modified: 29/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 
 import SwiftUI
 
 struct PlayerRow: View {
     @Binding var player: Player
     var body: some View {
+        //create a row showing name, score, and number of chips player has
         RoundedRectangle(cornerRadius: 10)
             .fill(myColor.blue)
             .frame(height: 50.0)
