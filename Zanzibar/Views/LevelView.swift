559c95c8483d8553c071f9c6452ba4da3b924e18
IDS-V-Intern-Uyennmd <67620752+uyennmd@users.noreply.github.com>
2022-08-29 22:24:31 +0700

    Update LevelView.swift

--- Zanzibar/Views/LevelView.swift
+++ Zanzibar/Views/LevelView.swift
@@ -1,9 +1,14 @@
-//
-//  LevelView.swift
-//  Zanzibar (iOS)
-//
-//  Created by Uyen Nguyen Minh Duy on 25/08/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Nguyen Minh Duy Uyen
+  ID: s3819342
+  Created  date: 23/08/2022
+  Last modified: 29/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 
 import SwiftUI
 
@@ -12,6 +17,7 @@ struct LevelView: View {
     @State var players = [Player]()
     
     var body: some View {
+        //View to redirect to correct view in setting 
         if (setting.level == "easy") {
             EasyPlay(players: createPlayers(numPlayer: setting.player, numChip: setting.chip))
         } else if (setting.level == "normal") {
