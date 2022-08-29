c416d0124a68c9ee418080a87b4ce99596efeca6
IDS-V-Intern-Uyennmd <67620752+uyennmd@users.noreply.github.com>
2022-08-29 22:06:44 +0700

    Update ButtonView.swift

--- Zanzibar/Views/ButtonView.swift
+++ Zanzibar/Views/ButtonView.swift
@@ -1,9 +1,14 @@
-//
-//  ButtonView.swift
-//  Zanzibar (iOS)
-//
-//  Created by Uyen Nguyen Minh Duy on 25/08/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Nguyen Minh Duy Uyen
+  ID: s3819342
+  Created  date: 23/08/2022
+  Last modified: 29/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 
 import SwiftUI
 
