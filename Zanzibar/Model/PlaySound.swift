234e6be5c8df11e1a667c14f7bdbb1def229e033
IDS-V-Intern-Uyennmd <67620752+uyennmd@users.noreply.github.com>
2022-08-29 22:02:19 +0700

    Update PlaySound.swift

--- Zanzibar/Model/PlaySound.swift
+++ Zanzibar/Model/PlaySound.swift
@@ -1,9 +1,14 @@
-//
-//  PlaySound.swift
-//  RMIT Casino
-//
-//  Created by Tom Huynh on 8/2/22.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Nguyen Minh Duy Uyen
+  ID: s3819342
+  Created  date: 23/08/2022
+  Last modified: 29/08/2022
+  Acknowledgement: https://github.com/TomHuynhSG/RMIT-Casino 
+*/
 
 import AVFoundation
 
